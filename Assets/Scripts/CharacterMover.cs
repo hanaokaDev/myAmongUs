1dc4d193aaf8aa92dd032a09e21b881bfcb232e0
user <user@gmail.com>
2025-03-02 00:27:38 +0900

    007 이동구현완료. 마우스로 움직이면 광속사출되는 버그있었는데 잡느라 오래걸림;;

---
+++ Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CharacterMover : NetworkBehaviour
+{
+    public bool isMovable;
+    [SyncVar] // Network로 동기화되도록
+    public float speed = 2f;
+
+    void Start()
+    {
+        if(isOwned){
+            Camera cam = Camera.main;
+            cam.transform.SetParent(transform);
+            cam.transform.localPosition = new Vector3(0f, 0f, -10f);
+            cam.orthographicSize = 2.5f; // 카메라 줌인
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move(){
+        if(isOwned && isMovable){
+            Debug.Log("foo 001");
+            if(PlayerSettings.controlType == EControlType.KeyboardMouse){
+                Debug.Log("foo 002");
+                Vector3 dir = Vector3.ClampMagnitude(
+                    new Vector3(
+                        Input.GetAxis("Horizontal"), 
+                        Input.GetAxis("Vertical"),
+                        0f
+                    ), 
+                    1f
+                ); 
+
+                if(dir.x < 0f) transform.localScale = new Vector3(-0.5f, 0.5f, 1f); // 좌우반전
+                else if(dir.x > 0f) transform.localScale = new Vector3(0.5f, 0.5f, 1f); 
+                transform.position += dir * speed * Time.deltaTime;
+            }
+            else{
+                Debug.Log("foo 003");
+                if(Input.GetMouseButton(0)){
+                    Debug.Log("foo 004");
+                    Debug.Log("Input.mousePosition: " + Input.mousePosition);
+                    Debug.Log("Screen.width: " + Screen.width);
+                    Debug.Log("Screen.height: " + Screen.height);
+                    Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f)).normalized;
+                    Debug.Log("dir: " + dir);
+                    if(dir.x < 0f) transform.localScale = new Vector3(-0.5f, 0.5f, 1f); // 좌우반전
+                    else if(dir.x > 0f) transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+                    Debug.Log("transform.localScale" + transform.localScale);
+                    transform.position += dir * speed * Time.deltaTime;
+                    Debug.Log("transform.position" + transform.position);
+                }
+            }
+
+        }
+    }
+}

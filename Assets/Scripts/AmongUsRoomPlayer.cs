5419eea5b1cfb021a00615c60e2c80a9aeb18fae
user <user@gmail.com>
2025-03-02 15:49:26 +0900

    Removed comments

--- Assets/Scripts/AmongUsRoomPlayer.cs
+++ Assets/Scripts/AmongUsRoomPlayer.cs
@@ -10,9 +10,9 @@ public class AmongUsRoomPlayer : NetworkRoomPlayer
 
     public void SpawnLobbyPlayerCharacter()
     {
-        Vector3 spawnPos = FindFirstObjectByType<SpawnPositions>().GetSpawnPosition(); // SpawnPositions 스크립트를 찾아서, GetSpawnPosition 함수를 호출하여 스폰 위치를 가져온다.
+        Vector3 spawnPos = FindFirstObjectByType<SpawnPositions>().GetSpawnPosition();
 
-        var player = Instantiate(AmongUsRoomManager.singleton.spawnPrefabs[0], spawnPos, Quaternion.identity); // AmongUsPlayer 프리팹을 인스턴스화. 1번째와 2번째 매개변수는 optional함.
+        var player = Instantiate(AmongUsRoomManager.singleton.spawnPrefabs[0], spawnPos, Quaternion.identity);
         NetworkServer.Spawn(player, connectionToClient); 
     }
 }

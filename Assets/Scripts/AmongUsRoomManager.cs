0cb0ac1ce502a5522f40271b397aa59bb368db70
user <user@gmail.com>
2025-03-07 08:10:47 +0900

    010 게임대기실최대인원 Host에 업데이트되도록 변경

--- Assets/Scripts/AmongUsRoomManager.cs
+++ Assets/Scripts/AmongUsRoomManager.cs
@@ -6,6 +6,9 @@ using Mirror;
 
 public class AmongUsRoomManager : NetworkRoomManager
 {
+    public int minPlayerCount;
+    public int imposterCount;
+
     // 서버에서 새로 접속한 클라이언트를 감지했을 때 동작하는 함수.
     public override void OnRoomServerConnect(NetworkConnectionToClient conn)
     {
--- Assets/Scripts/CreateRoomUI.cs
+++ Assets/Scripts/CreateRoomUI.cs
@@ -105,8 +105,10 @@ public class CreateRoomUI : MonoBehaviour
     }
     public void CreateRoom()
     {
-        var manager = AmongUsRoomManager.singleton;
-        // TODO: 방 설정 작업 처리
+        var manager = NetworkManager.singleton as AmongUsRoomManager;
+        manager.minPlayerCount = roomData.imposterCount == 1 ? 4 : roomData.imposterCount == 2 ? 7 : 9;
+        manager.imposterCount = roomData.imposterCount;
+        manager.maxConnections = roomData.maxPlayerCount;
         manager.StartHost(); // 서버를 여는 동시에 클라이언트로써 게임에 참가하도록 만들어주는 함수.
     }
 }

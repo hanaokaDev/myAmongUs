5528ef6cfe593e19df7e583ad581afc362e968fe
user <user@gmail.com>
2025-03-11 09:30:13 +0900

    012 Added EngineSteam: 연기가 사라지기 전 잠깐 멈칫하는 버그가 있음.

---
+++ Assets/Scripts/EngineBody.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineBody : MonoBehaviour
+{
+    [SerializeField]
+    public List<GameObject> steams = new List<GameObject>();
+
+    void Start()
+    {
+        foreach(var steam in steams){
+            StartCoroutine(RandomSteam(steam));
+        }
+    }
+
+    private IEnumerator RandomSteam(GameObject steam)
+    {
+        while(true){
+            float timer = Random.Range(0.5f, 1.5f);
+            while(timer >= 0f){
+                yield return null;
+                timer -= Time.deltaTime;
+            }
+
+            steam.SetActive(true);
+
+            timer = 0f;
+            while(timer <= 0.6f){
+                yield return null;
+                timer += Time.deltaTime;
+            }
+            steam.SetActive(false);
+        }
+    }
+
+}

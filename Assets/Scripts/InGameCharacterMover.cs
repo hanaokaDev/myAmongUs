ccfdd6e63ce40b8f743c17dcd3da5d5f0edbf9b7
user <user@gmail.com>
2025-03-14 08:54:24 +0900

    013 InGameCharacterMover에서 플레이어의 캐릭터 정보를 설정하는 CmdSetPlayerCharacter 메서드 추가 및 Start 메서드 수정

--- Assets/Scripts/InGameCharacterMover.cs
+++ Assets/Scripts/InGameCharacterMover.cs
@@ -6,5 +6,21 @@ using UnityEngine.UI;
 
 public class InGameCharacterMover : CharacterMover
 {
-    
+    public override void Start()
+    {
+        base.Start();
+        if(isOwned)
+        {
+            IsMovable = true;
+            var myRoomPlayer = AmongUsRoomPlayer.MyRoomPlayer;
+            CmdSetPlayerCharacter(myRoomPlayer.nickname, myRoomPlayer.playerColor);
+        }
+    }
+
+    [Command]
+    void CmdSetPlayerCharacter(string nickname, EPlayerColor playerColor)
+    {
+        this.nickname = nickname;
+        this.playerColor = playerColor;
+    }
 }

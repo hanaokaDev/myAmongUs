13cfb50cce3eef7fc067e89f6efad993132319a0
user <user@gmail.com>
2025-03-15 11:14:08 +0900

    014 아직 ShowPlayerType 미구현

---
+++ Assets/Scripts/InGameIntroUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameIntroUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text playerType;
+
+    [SerializeField]
+    private Image gradientImage;
+
+    [SerializeField]
+    private IntroCharacter myCharacter;
+
+    [SerializeField]
+    private Color crewColor;
+
+    [SerializeField]
+    private Color imposterColor;
+
+    public void ShowPlayerType()
+    {
+        
+    }
+
+
+}

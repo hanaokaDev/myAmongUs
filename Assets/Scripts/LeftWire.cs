74a7c8a16d83bd604241fd8123113bb43eb89390
hanaoka2481 <hanaoka2481@gmail.com>
2025-04-06 14:02:53 +0900

    025 04:24 마우스 클릭하여 드래그해도 전선이 안움직임;;

--- Assets/Scripts/LeftWire.cs
+++ Assets/Scripts/LeftWire.cs
@@ -5,4 +5,57 @@ using UnityEngine.UI;
 
 public class LeftWire : MonoBehaviour
 {   
+    [SerializeField]
+    private RectTransform mWireBody;
+
+    private LeftWire mSelectedWire;
+
+    [SerializeField]
+    private float offset = 15f;
+
+    private Canvas mGameCanvas; // 해상도가 달라질때마다 달라지는 Canvas 크기로 인한 distance 변동을 보정해주기 위함.
+
+    void Start()
+    {
+        mGameCanvas = FindObjectOfType<Canvas>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector2.right, 1f);
+            if(hit.collider != null)
+            {
+                var left = hit.collider.GetComponent<LeftWire>();
+                if(left != null)
+                {
+                    mSelectedWire = left;
+                }
+            }
+        }
+
+        // 마우스 떼면 원상복구
+        if(Input.GetMouseButton(0) && mSelectedWire != null)
+        {
+            if(mSelectedWire != null)
+            {
+                mWireBody.localRotation = Quaternion.Euler(Vector3.zero);
+                mWireBody.sizeDelta = new Vector2(0f, mWireBody.sizeDelta.y);
+                mSelectedWire = null; // 마우스 떼면 selectedWire 를 비워준다.
+            }
+        }
+
+        if(mSelectedWire != null)
+        {
+            float angle = Vector2.SignedAngle(transform.position + Vector3.right - transform.position, Input.mousePosition - transform.position); // 왜 transform.position 더했다 뺌?
+            float distance = Vector2.Distance(mWireBody.transform.position, Input.mousePosition) - offset;
+            mWireBody.localRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+            mWireBody.sizeDelta = new Vector2(distance * (1/mGameCanvas.transform.localScale.x), mWireBody.sizeDelta.y);
+
+
+        }
+    }
+
+
 }
